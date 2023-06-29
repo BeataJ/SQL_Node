0402ed349e0ac771a2ca9898e845cdd725d7a94b
beataj <beata@jasinski.com>
2023-06-28 18:28:13 -0700

    correct if

--- routes/blog.js
+++ routes/blog.js
@@ -8,13 +8,18 @@ router.get('/', (req, res) => {
     res.redirect('/posts')
 } );
 
-router.get("/posts", (req, res) => {
-    res.render('posts-list');
+router.get('/posts', async (req, res) => {
+    const query = `
+    SELECT posts.*, authors.name AS author_name FROM posts INNER JOIN authors ON posts.author_id = authors.id
+    `;
+    const [posts] = await db.query(query);
+    
+    res.render('posts-list', { posts: posts });
 });
 
 router.get('/new-post', async (req, res)=> {
     const [authors] = await db.query('SELECT * FROM authors');
-    res.render('create-post', { authors: authors});
+    res.render("create-post", { authors: authors });
 })
 
 router.post('/posts', async (req, res) => {
